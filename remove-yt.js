61bf117e7a832709fd836a2751dd786ca5de0e57
ludiofines <ludiofines@users.noreply.github.com>
2016-04-14 18:25:38 +0200

    add changelog

--- remove-yt.js
+++ remove-yt.js
@@ -6,3 +6,7 @@ if (document.getElementById('yt-consent-dialog') != null)
 {
 	document.getElementById('yt-consent-dialog').remove();
 }
+if (document.getElementById('feed-privacy-lb') != null)
+{
+	document.getElementById('feed-privacy-lb').remove();
+}
